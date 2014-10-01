f2c038e4a1520fc4bb73019854be718ce927b80c
Adam Back <adb33@email.arizona.edu>
2014-09-30 20:17:48 -0700

    Moved tools into a data file, added as script to load on index.html, and replaced them in the creation of the collection.

---
+++ data/data.js
@@ -0,0 +1,32 @@
+var tools = [
+  {
+    resource: 'foot',
+    type: "I",
+    allocated: 50
+  },
+
+  { 
+    resource: 'horse',
+    type: "II",
+    allocated: 2
+  },
+      
+  {
+   resource: 'helicopter',
+   type: "I",
+   allocated: 1
+  }, 
+    
+  {
+  resource: 'K9',
+  type: "II",
+  allocated: 3
+  },
+
+  {
+    resource: 'overhead staff',
+    type: "IV",
+    allocated: 10
+  }
+  
+];
